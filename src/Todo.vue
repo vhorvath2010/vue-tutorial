7d5d285b429daf0c61de80f596a835c047e5d160
Vince <vhorvath2010@gmail.com>
2022-04-27 13:45:26 -0700

    added strikethrough and hiding of completed items

--- src/Todo.vue
+++ src/Todo.vue
@@ -6,18 +6,31 @@ export default {
     data() {
         return {
             newTodo: '',
-            todos: []
+            todos: [],
+            hideCompleted: false,
+        }
+    },
+    computed: {
+        filteredTodos() {
+            if (this.hideCompleted) {
+                return this.todos.filter(todo => {
+                    return !todo.completed;
+                })
+            } else {
+                return this.todos;
+            }
         }
     },
     methods: {
         addTodo() {
-            this.todos.push({ id: id++, text: this.newTodo })
+            this.todos.push({ id: id++, text: this.newTodo, completed: false })
             this.newTodo = '';
         },
         removeTodo(todoToRemove) {
-            this.todos = this.todos.filter(todo => {
-                return todoToRemove !== todo;
-            })
+            this.todos = this.todos.filter(todo => { return todoToRemove !== todo; })
+        },
+        toggleHideCompleted() {
+            this.hideCompleted = !this.hideCompleted;
         }
     }
 }
@@ -26,12 +39,24 @@ export default {
 
 <template>
     <h1>Todo List</h1>
-    <input v-model="newTodo">
-    <button @click="addTodo">Add to List</button>
+    <form @submit.prevent="addTodo">
+        <input v-model="newTodo">
+        <button>Add to List</button>
+    </form>
     <ul>
-        <li v-for="todo in todos" :key="todo.id">
-            {{ todo.text }}
+        <li v-for="todo in filteredTodos" :key="todo.id">
+            <input type="checkbox" v-model="todo.completed">
+            <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
             <button @click="removeTodo(todo)">X</button>
         </li>
     </ul>
+    <button @click="toggleHideCompleted">
+        {{ hideCompleted ? "Show all" : "Hide Completed" }}
+    </button>
 </template>
+
+<style scoped>
+.completed {
+    text-decoration: line-through;
+}
+</style>
